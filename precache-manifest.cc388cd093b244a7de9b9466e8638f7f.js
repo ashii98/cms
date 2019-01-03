df9874529a9bf9f4fa95d12099bef9ca11466cab
Ashkan <dhs.ashkan@gmail.com>
2019-01-03 23:34:37 +0330

    Updates

---
+++ precache-manifest.cc388cd093b244a7de9b9466e8638f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cms/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66a0e7cdf4f78661ad7f",
+    "url": "/cms/static/js/runtime~main.66a0e7cd.js"
+  },
+  {
+    "revision": "f7340f01542b5daba98e",
+    "url": "/cms/static/js/main.f7340f01.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/cms/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f7340f01542b5daba98e",
+    "url": "/cms/static/css/main.3b1eb317.chunk.css"
+  },
+  {
+    "revision": "9708c5bde1db18f7d91ad5d376bbe210",
+    "url": "/cms/index.html"
+  }
+];
